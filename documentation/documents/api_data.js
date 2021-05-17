d977660420a8550ae79e8cfd4be05e34a6796347
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-16 23:18:37 -0300

    [Enhancement] Documentation - Generating Get User By ID documentation

---
+++ documentation/documents/api_data.js
@@ -0,0 +1,59 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "api/user/:id",
+    "title": "Get user by id",
+    "name": "GetUser",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Users unique ID.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Request status.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Email of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>Name of the User.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "endpoints/user.php",
+    "groupTitle": "User"
+  }
+] });
---
+++ documentation/documents/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Serie Login",
+  "version": "2.0.0",
+  "description": "API that gives access to a user CRUD.",
+  "title": "PHP API REST",
+  "url": "http://localhost/projects/PHP_API_REST/public_html/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-05-17T02:16:10.096Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
